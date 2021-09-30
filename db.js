53317bef2706352e4e805777a8395681813489f2
Jackie Lai <jlai@bbbsci.org>
2021-09-30 19:22:56 -0400

    Pushing server to heroku...again

--- db.js
+++ db.js
@@ -6,7 +6,13 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
-        dialect: 'postgres'
+        dialect: 'postgres', 
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        }
     }
 )
 
